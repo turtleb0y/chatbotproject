b21d2f38a29949fbab3ee70213d81fafb3986579
Miguel Angel Castillo <321angelcastillo.mc@gmail.com>
2016-09-23 11:01:09 -0700

    I am trying to make the thing talk.

---
+++ chatbot.js
@@ -0,0 +1,11 @@
+function myFunction() {
+  document.getElementById("chat-area").textContent = document.getElementById("question-area").value;
+}
+
+//var planetLife = {
+//  "mars" : "no",
+//  "earth" : "yes",
+//  "mercury" : "no",
+//}
+//var life = planetLife["earth"],
+//var userInput = "hello",
