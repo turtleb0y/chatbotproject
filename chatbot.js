c58c97a8ec158cddb9654f98e9b19a336907df2a
Miguel Angel Castillo <321angelcastillo.mc@gmail.com>
2016-09-29 09:15:43 -0700

    I am trying to progress

--- chatbot.js
+++ chatbot.js
@@ -1,15 +1,15 @@
-var speak
+//var speak
 
-var chatBot = {
-    "Hello" : "Hi there!",
-    "How are you?" : "I am great.",
-    "How's your day?" : "I don't know, I'm just sitting here all day."
-}
-
+//var chatBot = {
+//    "Hello" : "Hi there!",
+//    "How are you?" : "I am great.",
+//    "How's your day?" : "I don't know, I'm just sitting here all day."
+//}
+// I have no idea what i was doing here. . .
+//function myFunction() {
+//speak = document.getElementById("talkInput").value
 function myFunction() {
-speak = document.getElementById("talkInput").value
-
-if (chatBot) {
-
-}
+  var input = document.getElementById("talkInput").value;
+  var giveresponse = response[input]
+  document.getElementById("chat-area").innerHTML += input + ": " + giveresponse + " ";
 }
