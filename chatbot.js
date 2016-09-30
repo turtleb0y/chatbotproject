63ff32ade5e3e55601c2407a0a0f2ab588ea0272
Miguel Angel Castillo <321angelcastillo.mc@gmail.com>
2016-09-30 11:17:08 -0700

    This is coming along. . .

--- chatbot.js
+++ chatbot.js
@@ -8,8 +8,23 @@
 // I have no idea what i was doing here. . .
 //function myFunction() {
 //speak = document.getElementById("talkInput").value
+var response = {
+  "how are you?" : "I am doing well. Thank you for asking." + "<br>",
+  "how's life?" : "It's ok I guess, I just here all day." + "<br>",
+  "what do you do in your free time?" : "Nothing much, I just sit here all day, waiting . . ." + "<br>",
+  "how old are you?" : "I'M OVER 9000!" + "<br>",
+  "how is the world going to end?" : "I'll let you find that out . . ." + "<br>",
+}
+
 function myFunction() {
   var input = document.getElementById("talkInput").value;
   var giveresponse = response[input]
-  document.getElementById("chat-area").innerHTML += input + ": " + giveresponse + " ";
+
+
+  if(giveresponse == undefined) {
+    $("#chat-area").prepend("I do not understand your usage of the English language." + "<br>")
+  }
+  else{
+    $("#chat-area").prepend(input + ": " + giveresponse + " ");
+  }
 }
